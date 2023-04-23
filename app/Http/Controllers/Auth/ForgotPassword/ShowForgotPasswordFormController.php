4de338df29bbb4fc457cb2730cb350c795986411
Higor Grigorio <higorgrigorio@gmail.com>
2023-04-23 20:08:30 -0300

    refactor: Created the ShowForgotPasswordForm controller.

---
+++ app/Http/Controllers/Auth/ForgotPassword/ShowForgotPasswordFormController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Auth\ForgotPassword;
+
+use App\Core\Infra\IController;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+
+class ShowForgotPasswordFormController extends Controller implements IController
+{
+    /**
+     * @inheritDoc
+     */
+    public function handle(Request $request)
+    {
+        return View::make('auth.passwords.email');
+    }
+}
