9940510afb8184f52a11b88feab7ad07ac512001
Higor Grigorio <higorgrigorio@gmail.com>
2023-05-02 07:32:49 -0300

    feat: traits was moved to utility folder.

--- app/Core/Infra/Traits/HasBrokerMethods.php
+++ app/Core/Infra/Traits/HasBrokerMethods.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Core\Infra\Traits;
 
 use Illuminate\Contracts\Auth\PasswordBroker;
 use Illuminate\Support\Facades\Password;
--- app/Core/Infra/Traits/HasGuardMethods.php
+++ app/Core/Infra/Traits/HasGuardMethods.php
@@ -1,13 +1,14 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace App\Core\Infra\Traits;
 
 use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Contracts\Auth\StatefulGuard;
 use Illuminate\Support\Facades\Auth;
 
 trait HasGuardMethods
 {
-    protected function guard(): Guard|\Illuminate\Contracts\Auth\StatefulGuard
+    protected function guard(): Guard|StatefulGuard
     {
         return Auth::guard();
     }
