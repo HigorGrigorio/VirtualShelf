60ae98cce2ae68212a072854cfc8082cc3e3c122
Higor Grigorio <higorgrigorio@gmail.com>
2023-05-02 08:56:49 -0300

    feat: Confirm password was implemented.

--- app/Http/Controllers/Auth/ConfirmPassword/ConfirmPasswordController.php
+++ app/Http/Controllers/Auth/ConfirmPassword/ConfirmPasswordController.php
@@ -3,17 +3,47 @@
 namespace App\Http\Controllers\Auth\ConfirmPassword;
 
 use App\Core\Infra\IController;
+use App\Core\Infra\Traits\AlertsUser;
+use App\Core\Infra\Traits\HasValidator;
+use App\Core\Infra\Traits\RedirectsUser;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class ConfirmPasswordController extends Controller implements IController
 {
+    use HasValidator, AlertsUser, RedirectsUser;
 
     /**
-     * @inheritDoc
+     * @throws ValidationException
      */
-    public function handle(Request $request)
+    public function handle(Request $request): JsonResponse|RedirectResponse
     {
-        // TODO: Implement handle() method.
+        $this->validator($request->all())->validate();
+
+        $this->resetPasswordConfirmationTimeout($request);
+
+        return $request->wantsJson()
+            ? new JsonResponse([], 204)
+            : redirect()->intended($this->redirectPath());
+    }
+
+    protected function resetPasswordConfirmationTimeout(Request $request): void
+    {
+        $request->session()->put('auth.password_confirmed_at', time());
+    }
+
+    /**
+     * Get the password confirmation validation rules.
+     *
+     * @return array
+     */
+    protected function rules(): array
+    {
+        return [
+            'password' => 'required|current_password:web',
+        ];
     }
 }
