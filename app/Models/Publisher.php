fb93d1c6abdd3195f5b2bb60a04a9ddba8236602
Higor Grigorio <higorgrigorio@gmail.com>
2023-05-11 08:43:05 -0300

    feat: A fillable state_id was added

--- app/Models/Publisher.php
+++ app/Models/Publisher.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Database\Factories\PublisherFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Publisher extends Model
 {
@@ -12,5 +14,16 @@ class Publisher extends Model
     protected $fillable = [
         'name',
         'email',
+        'state_id'
     ];
+
+    protected function newFactory(): PublisherFactory
+    {
+        return new PublisherFactory();
+    }
+
+    public function state(): BelongsTo
+    {
+        return $this->belongsTo(State::class);
+    }
 }
